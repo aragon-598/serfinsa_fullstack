1bca9cafc1f08f47700c1e58710292fa8151235c
Alejandro Aragon <aragondru155@gmail.com>
2025-09-17 22:08:57 -0600

    feat: update app configuration and routing for improved navigation and interceptor integration

--- inventario-frontend/src/app/app.routes.ts
+++ inventario-frontend/src/app/app.routes.ts
@@ -7,10 +7,11 @@ import { ProductDetailComponent } from './product/product-detail.component';
 import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-	{ path: '', component: HomeComponent },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'productos', component: ProductListComponent, canActivate: [AuthGuard] },
-	{ path: 'productos/nuevo', component: ProductFormComponent, canActivate: [AuthGuard] },
-	{ path: 'productos/:id', component: ProductDetailComponent, canActivate: [AuthGuard] },
-	{ path: '**', redirectTo: '' }
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'productos', component: ProductListComponent, canActivate: [AuthGuard] },
+  { path: 'productos/nuevo', component: ProductFormComponent, canActivate: [AuthGuard] },
+  { path: 'productos/:id', component: ProductDetailComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
